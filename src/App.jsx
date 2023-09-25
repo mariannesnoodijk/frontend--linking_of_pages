7d95491aace20f9d2a85c43cb88664dacb71655b
msnoodijk001 <ams.snoodijk@gmail.com>
2023-09-25 16:25:00 +0200

    opdracht af

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import Home from './pages/home/Home.jsx';
 import Appointments from './pages/appointments/Appointments.jsx';
 import Cavities from './pages/cavities/Cavities.jsx';
 import Navigation from './components/navigation/./Navigation.jsx';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <>
             <Navigation />
-            <Home />
-            <Whitening />
-            <Appointments />
-            <Cavities />
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/whitening" element={<Whitening />}/>
+                <Route path="/appointments" element={<Appointments />}/>
+                <Route path="/cavities" element={<Cavities />}/>
+            </Routes>
         </>
     );
 }
